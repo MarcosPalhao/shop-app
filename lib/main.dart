3c246167747d58c0ad9c48802f5308f5ad9add7c
MarcosPalhao <75809289+MarcosPalhao@users.noreply.github.com>
2022-08-08 16:55:02 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:shop/pages/product_detail_page.dart';
+import 'package:shop/pages/products_overview_page.dart';
+import 'package:shop/utils/app_routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
+      ),
+      home: ProductOverviewPage(),
+      routes: {
+        AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage()
+      },
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
